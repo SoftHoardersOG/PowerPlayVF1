66ee257f41f375cbf41fb77a51818050174f385f
SoftHoarders <s.hoarders@gmail.com>
2022-10-30 13:31:54 +0200

    slow movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Movement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Movement.java
@@ -7,17 +7,25 @@ import org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive;
 
 public class Movement {
     private static SampleMecanumDrive drive;
-    public static void init(SampleMecanumDrive _drive){
-        drive=_drive;
+    private static int limitSpeed;
+
+    public static void init(SampleMecanumDrive _drive) {
+        drive = _drive;
     }
-    public static void run (Gamepad gamepad1){
+
+    public static void run(Gamepad gamepad1) {
+        setLimitSpeed(gamepad1);
         drive.setWeightedDrivePower(
                 new Pose2d(
-                        -gamepad1.right_stick_y,
-                        -gamepad1.right_stick_x,
-                        -gamepad1.left_stick_x
+                        -gamepad1.right_stick_y / limitSpeed,
+                        -gamepad1.right_stick_x / limitSpeed,
+                        -gamepad1.left_stick_x / limitSpeed
                 )
         );
         drive.update();
+    }
+
+    private static void setLimitSpeed(Gamepad gamepad) {
+        limitSpeed = gamepad.right_bumper ? 2 : 1;
     }
 }
